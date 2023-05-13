70383c498506e0f16b918ce97a2806f3475fb406
Aaron Frazier Jr <afraz91@wgu.edu>
2023-05-12 21:25:37 -0700

    Update main.java

--- main.java
+++ main.java
@@ -15,3 +15,17 @@ class concat {
         System.out.println(new_string);
     }
 }
+class concat {
+    public static void main(String[] args) {
+        String one = "Hello";
+        String two = "World";
+        String lower = "hello";
+        String same = "Hello";
+
+        System.out.println(one.equals(two));
+
+        System.out.println(one.equals(lower));
+
+        System.out.println(one.equals(same));
+    }
+}
